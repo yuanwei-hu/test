6c73bb4a2fd78d50057c77d17111be713abf25f0
胡远威 <huyuanwei@huyuanweidemac-mini.local>
2017-03-20 15:07:45 +0800

    Initial Commit

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by 胡远威 on 2017/3/20.
+//  Copyright © 2017年 胡远威. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by 胡远威 on 2017/3/20.
+//  Copyright © 2017年 胡远威. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
